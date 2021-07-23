480db9924b477bc22c7e69b3ac7bbe9b10a50676
AJ Herget <ajherget@berkeley.edu>
2021-07-22 17:36:04 -0700

    new file

--- draw/urls.py
+++ draw/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('user', views.user, name='user'),
     path('<str:room_name>/', views.room, name='room'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def user(request):
+    return render(request, 'draw/user.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
